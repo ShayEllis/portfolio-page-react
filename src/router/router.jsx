f5668cf5bde104bbee68586b64a9d6a0e6ab223a
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-07-12 10:56:52 -0700

    switched to hashRouter for gh pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import App from '../App'
 import Home from '../../components/home/home'
 
-const router = createBrowserRouter([{
+const router = createHashRouter([{
   path: '/',
   element: <App />,
   children: [
