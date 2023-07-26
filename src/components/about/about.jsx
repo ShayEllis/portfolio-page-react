d1b61b64fefef61b484c77ad0db7cfadc053aae7
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-07-25 20:21:53 -0700

    adjusted about page element positions and strted working on 3d carousel

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,22 +3,22 @@ import linkedInIcon from '../../assets/linkedin-logo.png'
 import gitHubIcon from '../../assets/github-logo.png'
 
 const About = () => {
+  const translateZ = Math.round(
+    42 /* cell size */ / 2 / Math.tan(Math.PI / 4 /* # cells */)
+  )
+  console.log(translateZ)
+  const angle = 1/*selectedIndex*/ / 4/*cellCount*/ * -360
+  console.log(angle)
+
   return (
     <section id='about'>
       <h2>About</h2>
-      <div className='about-container'>
+      <div className='aboutContainer'>
         <p>
           My name is Shay Ellis. I am a web developer based in central Oregon. I
           am currently enrolled in a the Full-Stack Engineering course at
           codecademy.
         </p>
-        <div className='picture-container'>
-          <img
-            src={myPicture}
-            alt='Professional portrait of Shay Ellis'
-            className='my-picture'
-          />
-        </div>
         <div className='social-link-container'>
           <a href='https://github.com/ShayEllis' target='_blank'>
             <img src={gitHubIcon} />
@@ -27,16 +27,18 @@ const About = () => {
             <img src={linkedInIcon} />
           </a>
         </div>
-        <section id='projects'>
-          <h2>Projects</h2>
-          <div className='projects-container'>
-            <div className='project'></div>
-            <div className='project'></div>
-            <div className='project'></div>
-            <div className='project'></div>
-          </div>
-        </section>
       </div>
+      <section id='projects'>
+        <h2>Projects</h2>
+        <div className='scene'>
+          <div className='projectsCarousel'>
+            <div className='projectCell'>1</div>
+            <div className='projectCell'>2</div>
+            <div className='projectCell'>3</div>
+            <div className='projectCell'>4</div>
+          </div>
+        </div>
+      </section>
     </section>
   )
 }
