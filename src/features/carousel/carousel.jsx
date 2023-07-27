e485480cebe3f29dac3799fff0040cc545dc1edd
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-07-27 12:18:16 -0700

    made carosuel responsive

--- src/features/carousel/carousel.jsx
+++ src/features/carousel/carousel.jsx
@@ -1,9 +1,14 @@
-import { useRef, useEffect, useLayoutEffect, useState, useReducer } from 'react'
+import { useRef, useEffect, useLayoutEffect, useState } from 'react'
 import { gsap } from 'gsap'
 
 const Carousel = () => {
+  const [windowWidth, setWindowWidth] = useState(null)
   const scene = useRef()
   const projectsCarousel = useRef()
+
+  const handleResize = ({ target }) => {
+    setWindowWidth(target.innerWidth)
+  }
 
   useLayoutEffect(() => {
     const projectCells = [...projectsCarousel.current.children]
@@ -11,12 +16,16 @@ const Carousel = () => {
     const translateZ = Math.round(
       scene.current.offsetWidth / 2 / Math.tan(Math.PI / projectCells.length)
     )
-    console.log('translate', translateZ)
-    // Set carousel Z position and use rotateY to rotate carousel
-    projectsCarousel.current.style.transform = `translateZ(${-translateZ}px)`
-    // Set cell 3d placement
+    // Calculate cell rotateY position
+    const rotateIncrement = 360 / projectCells.length
+    // Set carousel Z position
+    const ctx = gsap.context(() => {
+      gsap.set(projectsCarousel.current, {
+        translateZ: -translateZ,
+      })
+    })
+    // Set each cells 3d placement using translateZ and rotateY
     projectCells.forEach((cell, index) => {
-      const rotateIncrement = 360 / projectCells.length
       if (index === 0) {
         cell.style.transform = `rotateY(0deg) translateZ(${translateZ}px)`
       } else {
@@ -24,21 +33,28 @@ const Carousel = () => {
           rotateIncrement * index
         }deg) translateZ(${translateZ}px)`
       }
+    })
+    // Add resize listener to window to rerender carousel dimentions
+    window.addEventListener('resize', handleResize)
 
-      const context = gsap.context(() => {
-        gsap.to(projectsCarousel.current, {
-          rotationY: '+=360',
-          duration: 4 * projectCells.length,
-          ease: 'none',
-          repeat: -1,
-        })
-      }, [scene.current])
+    return () => {
+      ctx.revert()
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [windowWidth])
 
-      return () => context.revert()
+  useEffect(() => {
+    // Use rotateY to rotate the entire carousel
+    const ctx = gsap.context(() => {
+      gsap.to(projectsCarousel.current, {
+        rotationY: '+=360',
+        duration: 4 * projectsCarousel.current.children.length,
+        ease: 'none',
+        repeat: -1,
+      })
     })
 
-    const angle = (1 /*selectedIndex*/ / 4) /*cellCount*/ * -360
-    console.log('angle', angle)
+    return () => ctx.revert()
   }, [])
 
   return (
@@ -47,6 +63,9 @@ const Carousel = () => {
         <div className='projectCell'>1</div>
         <div className='projectCell'>2</div>
         <div className='projectCell'>3</div>
+        <div className='projectCell'>4</div>
+        <div className='projectCell'>4</div>
+        <div className='projectCell'>4</div>
         <div className='projectCell'>4</div>
       </div>
     </div>
